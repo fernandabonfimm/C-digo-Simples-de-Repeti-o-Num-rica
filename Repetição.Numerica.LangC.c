b05da91c765eafc247b9eb983e1a3e0c680ddb27
fernandabonfimm <82246157+fernandabonfimm@users.noreply.github.com>
2021-10-16 17:41:23 -0300

    Add files via upload

---
+++ Repetição.Numerica.LangC.c
@@ -0,0 +1,36 @@
+// Lista de Exercicios 2: Questão 7 //
+// Author: Fernanda Bonfim - Cod: 2069922 //
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, recpto, result, rep, numeracao[30];
+
+printf("\n|--------------------------------------------|");
+printf("\n| Bem-Vindo ao Código de Repetição númerica! |");
+printf("\n|--------------------------------------------|");
+
+printf("\nDigite a Quantidade de repetições que deseja: \n");
+scanf("%d", &rep);
+printf("\nDigite os números que deseja, para a ordem númerica: \n");
+for (i = 0; i < rep; ++i)
+    scanf("%d", &numeracao[i]);
+for (i = 0; i < rep; ++i)
+{
+    for (recpto = i + 1; recpto < rep; ++recpto)
+    {
+        if (numeracao[i] > numeracao[recpto])
+        {
+            result =  numeracao[i];
+            numeracao[i] = numeracao[recpto];
+            numeracao[recpto] = result;
+        }
+    }
+}
+printf("A ordem Crescente dos números é: \n");
+for (i = 0; i < rep; ++i)
+    printf("%d\n", numeracao[i]);
+
+    return 0;
+}
